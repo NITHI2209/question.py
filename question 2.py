f75da7c15d65382710f755306a044f52864150aa
NITHI2209 <nithyasree065@gmail.com>
2025-07-10 23:49:21 +0530

    Create question 2.py

---
+++ question 2.py
@@ -0,0 +1,8 @@
+# star pattern 
+n = int(input("enter the number of rows: "))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end="")
+    print()
