500f7d4a6151d3c3a16654e2126462f102cebeec
NITHI2209 <nithyasree065@gmail.com>
2025-07-10 23:50:10 +0530

    Create question 3.py

---
+++ question 3.py
@@ -0,0 +1,7 @@
+n = int(input("enter the number of rows: "))
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end="")
+    print()
