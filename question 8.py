4ec6fe6e77dde117685ea357fd7baf17c94b6e0f
NITHI2209 <nithyasree065@gmail.com>
2025-07-11 19:44:10 +0530

    Update question 8.py

--- question 8.py
+++ question 8.py
@@ -1,5 +1,20 @@
+#question:
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5 
+#code:
 n = int(input("enter the number of rows: "))
 for i in range(1,n+1):
     for j in range(i):
         print(i,end=" ")
     print()
+#sample:
+# enter the number of rows: 6
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5 
+# 6 6 6 6 6 6 
