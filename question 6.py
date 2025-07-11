9d30638ce70ec1ccf098e24b2fb8787a2363fdb0
NITHI2209 <nithyasree065@gmail.com>
2025-07-11 19:41:43 +0530

    Update question 6.py

--- question 6.py
+++ question 6.py
@@ -1,7 +1,23 @@
- n = int(input("enter the number of rows: "))
+#question
+# * * * * * 
+#  * * * * 
+#   * * * 
+#    * * 
+#     * 
+#code
+n = int(input("enter the number of rows: "))
 for i in range(n):
     for j in range(i):
         print(" ",end="")
     for k in range(n-i):
         print("*",end=" ")
     print()
+ #sample:
+# enter the number of rows: 7
+# * * * * * * * 
+#  * * * * * * 
+#   * * * * * 
+#    * * * * 
+#     * * * 
+#      * * 
+#       * 
